054ae156413a8c2455a9a39e325415a96eadba4c
Dmitry Marchuk <code@dmarchuk.com>
2021-11-30 16:21:19 +0200

    add async main fix

--- index.ts
+++ index.ts
@@ -1,4 +1,11 @@
-import { main } from "./src/main";
+import {main} from "./src/main";
 
-main();
-process.stdout.write(" then index\n");
+(async () => {
+  try {
+    await main();
+    console.log("finish");
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+})();
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-export function main() {
-  process.stdout.write("main");
+export async function main() {
+  console.log("main()");
+  await delay(100);
+  console.log("wait 100ms");
 }
 
 export const sum = (...a: number[]) => a.reduce((acc, val) => acc + val, 0);
+
+export function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
